85bb08fc6ce66a6e0f72452b701bd228099a4fdb
robertotics4 <robertooliveiragp@gmail.com>
2024-08-26 00:26:00 -0300

    refactor: create game configurations

--- js/script.js
+++ js/script.js
@@ -2,11 +2,32 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
+// Configurations
+const gameConfigurations = {
+  canvasSize: canvas.width,
+  rectSize: 30,
+  generalVelocity: 300,
+  grid: {
+    lineWidth: 1,
+    lineColor: '#191919'
+  },
+  snake: {
+    bodyColor: '#ddd',
+    headColor: 'white',
+    initialPosition: { x: 270, y: 240 }
+  },
+  food: {
+    shadowSize: 6
+  }
+};
+
+// functions
 function randomNumber(min, max) {
   return Math.round(Math.random() * (max - min) + min);
 }
 
-function randomPosition(canvasSize, rectSize) {
+function randomPosition(gameConfigurations) {
+  const { canvasSize, rectSize } = gameConfigurations;
   const num = randomNumber(0, canvasSize - rectSize);
   return Math.round(num / rectSize) * 30;
 }
@@ -18,46 +39,23 @@ function randomColor() {
   return `rgb(${red}, ${green}, ${blue})`
 }
 
-function drawSnake(snake, rectSize) {
-  ctx.fillStyle = defaultBodySnakeColor;
+function drawSnake(snake, gameConfigurations) {
+  const { snake: snakeConfigurations, rectSize } = gameConfigurations;
+
+  ctx.fillStyle = snakeConfigurations.bodyColor;
 
   snake.forEach((position, index) => {
     if (index === snake.length - 1) {
-      ctx.fillStyle = defaultHeadSnakeColor;
+      ctx.fillStyle = snakeConfigurations.headColor;
     }
 
     ctx.fillRect(position.x, position.y, rectSize, rectSize);
   });
 }
 
-function drawGrid(gridLineWidth, gridLineColor, canvasSize) {
-  ctx.lineWidth = gridLineWidth;
-  ctx.strokeStyle = gridLineColor;
+function moveSnake(snake, gameConfigurations) {
+  const { rectSize } = gameConfigurations;
 
-  for (let i = 30; i < canvasSize; i += 30) {
-    ctx.beginPath();
-    ctx.lineTo(i, 0);
-    ctx.lineTo(i, canvasSize);
-    ctx.stroke();
-
-    ctx.beginPath();
-    ctx.lineTo(0, i);
-    ctx.lineTo(600, i);
-    ctx.stroke();
-  }
-}
-
-function drawFood(food, rectSize) {
-  const { x, y, color } = food;
-
-  ctx.shadowColor = color;
-  ctx.shadowBlur = defaultFoodShadowSize;
-  ctx.fillStyle = color;
-  ctx.fillRect(x, y, rectSize, rectSize);
-  ctx.shadowBlur = 0;
-}
-
-function moveSnake(snake, rectSize) {
   if (!direction) return;
 
   const head = snake[snake.length - 1];
@@ -81,42 +79,63 @@ function moveSnake(snake, rectSize) {
   snake.shift();
 }
 
-function runGameLoop() {
-  clearInterval(loopId);
+function drawGrid(gameConfigurations) {
+  const { grid, canvasSize } = gameConfigurations;
+  ctx.lineWidth = grid.lineWidth;
+  ctx.strokeStyle = grid.lineColor;
 
-  ctx.clearRect(0, 0, defaultCanvasSize, defaultCanvasSize);
+  for (let i = 30; i < canvasSize; i += 30) {
+    ctx.beginPath();
+    ctx.lineTo(i, 0);
+    ctx.lineTo(i, canvasSize);
+    ctx.stroke();
 
-  drawGrid(defaultGridLineWidth, defaultGridLineColor, defaultCanvasSize);
-  drawFood(food, defaultRectSize);
-  moveSnake(snake, defaultRectSize);
-  drawSnake(snake, defaultRectSize);
-
-  loopId = setTimeout(() => { runGameLoop() }, defaultGameVelocity);
+    ctx.beginPath();
+    ctx.lineTo(0, i);
+    ctx.lineTo(600, i);
+    ctx.stroke();
+  }
 }
 
-// Configurations
-const defaultCanvasSize = canvas.width;
-const defaultRectSize = 30;
-const defaultGameVelocity = 300;
-const defaultBodySnakeColor = '#ddd';
-const defaultHeadSnakeColor = 'white';
-const defaultGridLineWidth = 1;
-const defaultGridLineColor = '#191919';
-const defaultFoodColor = "yellow";
-const defaultFoodShadowSize = 6;
+function drawFood(food, gameConfigurations) {
+  const { food: foodConfigurations, rectSize } = gameConfigurations;
+  const { x, y, color } = food;
+
+  ctx.shadowColor = color;
+  ctx.shadowBlur = foodConfigurations.shadowSize;
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, rectSize, rectSize);
+  ctx.shadowBlur = 0;
+}
+
+function runGameLoop(gameConfigurations) {
+  const { canvasSize, generalVelocity } = gameConfigurations;
+
+  clearInterval(loopId);
+
+  ctx.clearRect(0, 0, canvasSize, canvasSize);
+
+  drawGrid(gameConfigurations);
+  drawFood(food, gameConfigurations);
+  moveSnake(snake, gameConfigurations);
+  drawSnake(snake, gameConfigurations);
+
+  loopId = setTimeout(() => {
+    runGameLoop(gameConfigurations)
+  }, generalVelocity);
+}
 
 // define objects
 let direction, loopId;
-const initialSnakePosition = { x: 270, y: 240 };
-const snake = [initialSnakePosition];
+const snake = [gameConfigurations.snake.initialPosition];
 const food = {
-  x: randomPosition(defaultCanvasSize, defaultRectSize),
-  y: randomPosition(defaultCanvasSize, defaultRectSize),
+  x: randomPosition(gameConfigurations),
+  y: randomPosition(gameConfigurations),
   color: randomColor()
 };
 
 
-runGameLoop();
+runGameLoop(gameConfigurations);
 
 document.addEventListener('keydown', event => {
   const { key } = event;
